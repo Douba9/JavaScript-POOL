86e37dd5a5799aad476cb55dfcfde78a2f8c0512
marius <marius.mestron@epitech.eu>
2022-10-03 15:04:13 +0200

    [ADD]: ex_04

---
+++ day-1/ex_04/server.js
@@ -0,0 +1,22 @@
+let config = require("../config.js");
+let app = config.app;
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send("");
+});
+
+app.get(`/name/:name`, (req, res) => {
+
+    let name = req.params.name;
+    res.render(`name`, {name: name});
+
+});
+
+app.get('/name/', (req, res) => {
+    res.render(`name`, {name: "unknown"});
+})
+  
+app.listen(config.port, () => {
+    console.log(`listen on [ip: ${config.ip}] - [port: ${config.port}]`);
+});
