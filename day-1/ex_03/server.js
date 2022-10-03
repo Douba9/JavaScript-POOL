a5831a8d09c187657185741dddb439c9306b8783
marius <marius.mestron@epitech.eu>
2022-10-03 15:03:47 +0200

    [EDIT]: ex_03

--- day-1/ex_03/server.js
+++ day-1/ex_03/server.js
@@ -3,8 +3,8 @@ let app = config.app
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`)
-  })
+})
   
-  app.listen(config.port, () => {
+app.listen(config.port, () => {
     console.log(`listen on [ip: ${config.ip}] - [port: ${config.port}]`)
-  })
+})
