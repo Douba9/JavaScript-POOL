03693eef941205a0d39e06edbea1d475d7e8ce54
marius <marius.mestron@epitech.eu>
2022-10-03 16:24:38 +0200

    [EDIT]

---
+++ ex_01/server.js
@@ -0,0 +1,10 @@
+let config = require("../config.js")
+let app = config.app
+
+app.get('/', (req, res) => {
+  res.send('')
+})
+
+app.listen(config.port, () => {
+  console.log(`port ${config.port}`)
+})
